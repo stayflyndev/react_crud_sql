a27e6bb340ebdc31c46ffee0dc1c29649bd57f5b
stayflyndev <aretee64@gmail.com>
2022-08-30 14:21:39 -0400

    modifying models

--- server/models/Events.js
+++ server/models/Events.js
@@ -1,21 +1,17 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const hotelSchema = new Schema({
-    title:
-    {
-        String,
-        required: true
-    },
+const eventSchema = new Schema({
+    title: String,
     author: String,
-    desc: {String, required: true},
+    desc: {String},
     distance: String, // String is shorthand for {type: String}
     comments: [{ body: String, date: Date }],
-    photos: [{
+    photos: [
         String
-    }],
-    tickets:[{String}],
-    price:{Number, required:true},
+    ],
+    tickets:[String],
+    price:{Number},
     featured:{Boolean, default: false},
     date: { type: Date, default: Date.now },
     hidden: Boolean,
@@ -24,3 +20,5 @@ const hotelSchema = new Schema({
         favs: Number
     }
 });
+
+module.exports = mongoose.model('Event', eventSchema)
--- server/routes/events.js
+++ server/routes/events.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const router = express.Router();
+const Event = require("../models/Events")
  
 //CREATE NEW EVENT
-router.post('/', (req, res) => {
-    
+router.post('/', async (req, res) => {
+
+    const newEvent = new Event(req.body)
+
+    try{
+        const savedEvent = await newEvent.save()
+        res.status(200).json(savedEvent)
+
+
+    }catch(err){
+        res.status(500).json(err)
+    }
 })
 
 
